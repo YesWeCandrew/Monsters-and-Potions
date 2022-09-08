9364918186964be589e9b3b847b01f154102d50b
Ethan Teber-Rossi <u7298300@anu.edu.au>
2022-09-08 18:09:46 +1000

    Implemented reader for contents of CSV files for constructing maps. Added dummySave.csv as a test. Currently prints to terminal.

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,9 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Map {
 
     /**
@@ -17,6 +21,90 @@ public class Map {
      * @param pathToJSON the path to the JSON or XML or whatever file
      */
     public Map(String pathToCSV,String pathToJSON) {
+
+        loadMapFromCSV(pathToCSV);
+
+    }
+
+    public static void main(String[] args) {
+        loadMapFromCSV("dummySave");
+    }
+
+
+    /**
+     * Constructs a blank game objectMap with walls based on the wall
+     * layout of the given map csv file.
+     * @param pathToCSV the path to the map csv file
+     * @return the corresponding GameObject map.
+     */
+    private static GameObject[][] loadMapFromCSV(String pathToCSV) {
+        
+        String line = "";
+        String splitBy = ",";
+        final String SAVE_FILE_PATH = "src//saves";
+
+
+        short size = 8;
+        GameObject[][] map = new GameObject[size][size];
+        String [][] printMap = new String[size][size];
+        short row = 0;
+
+
+        try
+        {
+        //Parse the CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(SAVE_FILE_PATH + "//" + pathToCSV +".csv"));
+
+            //Read each line, splitting into walls and spaces.
+            while ((line = br.readLine()) != null)
+            {
+                // Split the line into segments based on comma as separator
+                String[] gameRow = line.split(splitBy);
+
+                //Convert to game objects and place into the map.
+                for (int i = 0; i < gameRow.length && i < size; i++) {
+
+                    //Create "create gameObject function
+                    GameObject object = createGameObject();
+
+                    //temp map
+                    printMap[i][row] = gameRow[i];
+                    if (gameRow[i].equals("_")){
+                        printMap[i][row] = " ";
+                    }
+
+                }
+                row++;
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Printing out test map
+        for (int i = 0; i < printMap.length; i++) {
+            for (int j = 0; j < printMap.length; j++) {
+                System.out.print(printMap[j][i]);
+                if(j ==7){
+                    System.out.println("");
+                }
+            }
+        }
+
+
+
+        return map;
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public static GameObject createGameObject(){
+        GameObject retGameObject = null;
+        return retGameObject;
     }
 
     /**
