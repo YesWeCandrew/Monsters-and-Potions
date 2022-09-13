1436b59a2b3e4752a3121498abe2cb95c4452337
Ethan Teber-Rossi <u7298300@anu.edu.au>
2022-09-13 16:55:52 +1000

    CSV load and save complete.

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,12 @@
 package src;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.CSVWriter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map {
 
@@ -12,8 +16,11 @@ public class Map {
     Y = row
 
      */
-    private GameObject[][] map;
-
+    private static GameObject[][] map;
+    static final String SAVE_FILE_PATH = "src//saves";
+    private static final short size = 8;
+    private static final String WALL_CHARACTER = "X";
+    private static final String VACANT_CHARACTER = "_";
     /**
      * Generates the Map (and all the other objects using their relevant
      * constructors), using the csv and JSON (or other file type tbd) provided
@@ -22,12 +29,71 @@ public class Map {
      */
     public Map(String pathToCSV,String pathToJSON) {
 
-        loadMapFromCSV(pathToCSV);
+        GameObject[][] initialMap =  loadMapFromCSV(pathToCSV);
+        map = loadEntitiesFromJSON(initialMap,pathToJSON);
 
     }
 
+    private GameObject[][] loadEntitiesFromJSON(GameObject[][] initialMap, String pathToJSON) {
+        GameObject[][] retMap = initialMap;
+
+        //Load json file
+
+        //Go through file.
+
+
+        Hero hero = new Hero('H',"Jeff",100,100,null,null,"A hero.");
+        String jsonObject = writeJSONObject(hero);
+        Hero hero2 = new Hero('H',"Greg",200,100,null,null,"A greg.");
+        String jsonObject2 = writeJSONObject(hero);
+
+        String finalJson = jsonObject+jsonObject2;
+        writeJSONTOFile(finalJson);
+
+        return retMap;
+    }
+
+    private void writeJSONTOFile(String json){
+        GsonBuilder builder = new GsonBuilder().setPrettyPrinting();
+        Gson gson = builder.create();
+        try (FileWriter writer = new FileWriter(SAVE_FILE_PATH+"//newJsonFile.json")) {
+            gson.toJson(json, writer);
+            System.out.println("Written!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private String writeJSONObject(GameObject object){
+        GsonBuilder builder = new GsonBuilder().setPrettyPrinting();
+        Gson gson = builder.create();
+        String json = gson.toJson(object);
+        return json;
+    }
+
+    public static String readJSON(String filePath){
+        Gson gson = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(SAVE_FILE_PATH + "//" + filePath+".json"));
+            String result = gson.toJson(br);
+            return result;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     public static void main(String[] args) {
-        loadMapFromCSV("dummySave");
+        new Map("dummySave","");
+        //readJSON("newJsonFile");
+        map = loadMapFromCSV("dummySave");
+        saveMapCSV("greg");
+
     }
 
 
@@ -38,23 +104,19 @@ public class Map {
      * @return the corresponding GameObject map.
      */
     private static GameObject[][] loadMapFromCSV(String pathToCSV) {
-        
-        String line = "";
+
+
+        //Declare restricted size of the map
+        GameObject[][] returnMap = new GameObject[size][size];
+
+        //Initialise parameters for csv file
+        String line;
         String splitBy = ",";
-        final String SAVE_FILE_PATH = "src//saves";
-
-
-        short size = 8;
-        GameObject[][] map = new GameObject[size][size];
-        String [][] printMap = new String[size][size];
-        short row = 0;
-
-
         try
         {
         //Parse the CSV file into BufferedReader class constructor
             BufferedReader br = new BufferedReader(new FileReader(SAVE_FILE_PATH + "//" + pathToCSV +".csv"));
-
+            short row = 0;
             //Read each line, splitting into walls and spaces.
             while ((line = br.readLine()) != null)
             {
@@ -64,13 +126,10 @@ public class Map {
                 //Convert to game objects and place into the map.
                 for (int i = 0; i < gameRow.length && i < size; i++) {
 
-                    //Create "create gameObject function
-                    GameObject object = createGameObject();
-
-                    //temp map
-                    printMap[i][row] = gameRow[i];
-                    if (gameRow[i].equals("_")){
-                        printMap[i][row] = " ";
+                    if (gameRow[i].equals(WALL_CHARACTER)){
+                        returnMap[i][row] = new Wall();
+                    } else{
+                        returnMap[i][row] = null;
                     }
 
                 }
@@ -82,28 +141,72 @@ public class Map {
             e.printStackTrace();
         }
 
-        //Printing out test map
-        for (int i = 0; i < printMap.length; i++) {
-            for (int j = 0; j < printMap.length; j++) {
-                System.out.print(printMap[j][i]);
-                if(j ==7){
-                    System.out.println("");
+//        //Printing out test map
+//        for (int i = 0; i < map.length; i++) {
+//            for (int j = 0; j < map.length; j++) {
+//                if(map[j][i] instanceof Wall){
+//                    System.out.print("X");
+//                } else{
+//                    System.out.print("_");
+//                }
+//                if(j ==7){
+//                    System.out.println("");
+//                }
+//            }
+//        }
+
+        return returnMap;
+    }
+    /**
+     * Saves the current map as a CSV to local storage.
+     * @param fileName the name of the new CSV filesave.
+     */
+    public static void saveMapCSV(String fileName) {
+        //initialise string array to collect map data.
+        List<String[]> data = new ArrayList<>();
+
+        //Convert map to String 2D Array
+        for (int i = 0; i < size; i++) {
+            //Construct the CSV row.
+            String[] row = new String[size];
+            for (int j = 0; j < size; j++) {
+                if ((map[j][i] != null) && (map[j][i] instanceof Wall)) {
+                    row[j] = WALL_CHARACTER;
+                } else {
+                    row[j] = VACANT_CHARACTER;
                 }
             }
+            //Place in data array.
+            data.add(row);
         }
+        //Setup file
+        File file = new File(SAVE_FILE_PATH + "//" + fileName + ".csv");
+        try {
+            // Create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(file);
 
-
-
-        return map;
+            // Create CSVWriter with ',' as separator
+            CSVWriter writer = new CSVWriter(outputfile, ',',
+                    CSVWriter.NO_QUOTE_CHARACTER,
+                    CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                    CSVWriter.DEFAULT_LINE_END);
+            //Write data to CSV file.
+            writer.writeAll(data);
+            // Closing writer connection
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    /**
+        /**
      *
      * @return
      */
     public static GameObject createGameObject(){
         GameObject retGameObject = null;
+
         return retGameObject;
     }
 
