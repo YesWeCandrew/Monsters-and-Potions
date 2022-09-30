7e70ee79c3dfd7df612cb5ed08547becf3f21ed9
Andrew <Andrew.Howes@anu.edu.au>
2022-09-30 19:49:49 +1000

    Added tests for map

--- src/map/Map.java
+++ src/map/Map.java
@@ -8,11 +8,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -91,8 +87,18 @@ public class Map {
         loadXANDYSize(pathToCSV);
         loadMapFromCSV(pathToCSV);
         loadEntitiesFromJSON(pathToJSON);
-
     }
+
+    /**
+     * Generates the Map without any objects from CSV. Just for testing.
+     * @param pathToCSV the path to the csv file
+     * @author Andrew Howes
+     */
+    public Map(String pathToCSV) {
+        loadXANDYSize(pathToCSV);
+        loadMapFromCSV(pathToCSV);
+    }
+
 
     private static void loadXANDYSize(String pathToCSV) {
         obtainXSize(pathToCSV);
@@ -651,7 +657,7 @@ public class Map {
      * Will return false if:
      * - The position is off the board, or
      * - There is already an object at the given position
-     * To replace an object, first use the removeObjectAt function, then
+     * To replace an object, first use the clearPosition function, then
      * call this function.
      * @param position the position to place the object
      * @param object the object to place
@@ -659,12 +665,13 @@ public class Map {
      */
     public boolean setObjectAt(Position position, GameObject object) {
         boolean set = false;
-        if(isEmpty(position)) {
+        if (isEmpty(position)) {
             map[position.getX()][position.getY()] = object;
             set = true;
         }
 
         return set;
+    }
 
 //        // Wrapped in try block in case the position is off the board
 //        // The assert function also ensures that the catch is called if there
@@ -679,7 +686,7 @@ public class Map {
 //        } catch (Exception e) {
 //            return false;
 //        }
-    }
+
 
     /**
      * Sets the given position to null.
@@ -835,6 +842,9 @@ public class Map {
                     item.getName().equals("Amulet")) {
                 heroEscaped = true;
             }
+
+            // Remove the item from the map
+            if (success) {clearPosition(positionInFrontOfHero());}
 
             // regardless of whether you've won or died or just still going
             // returns success because you picked up an item!
@@ -995,4 +1005,62 @@ public class Map {
         }
         return allPossibleActions;
     }
+
+    public GameObject[][] getMap() {
+        return map;
+    }
+
+    public int getXSize() {
+        return X_SIZE;
+    }
+
+    public int getYSize() {
+        return Y_SIZE;
+    }
+
+    /**
+     * Iterates over each row and column to generate a string representation
+     * of the map
+     * @return the map as a string.
+     * @author Andrew Howes
+     */
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int y = 0; y < Y_SIZE; y ++) {
+            for (int x = 0; x < X_SIZE; x++) {
+                if (map[x][y] == null) {stringBuilder.append(" ");}
+                else {stringBuilder.append(map[x][y].getChar());}
+            }
+            if (y < Y_SIZE-1) {stringBuilder.append("\n");}
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Map map = (Map) o;
+        boolean heroEscapedBool = Objects.equals(heroEscaped, map.heroEscaped);
+        boolean heroPositionReferenceBool = Objects.equals(heroPositionReference, map.heroPositionReference);
+        boolean heroFacingBool = heroFacing == map.heroFacing;
+        boolean mapBool = X_SIZE == map.getXSize() && Y_SIZE == map.getYSize();
+
+        if (!(heroEscapedBool && heroPositionReferenceBool && heroFacingBool && mapBool)) {
+            return false;
+        }
+
+        for (int y = 0; y < Y_SIZE; y++) {
+            for (int x = 0; x < X_SIZE; x++) {
+                if (!(Objects.equals(getMap()[x][y],map.getMap()[x][y]))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
--- tests/map/MapTests.java
+++ tests/map/MapTests.java
@@ -3,24 +3,24 @@ package map;
 import objects.Hero;
 import objects.Item;
 import objects.Monster;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MapTests {
 
-    static Map testMap = new Map("dummySave","");
+    private static Map testMap;
 
     /*
-    testMap:
+    TestMap:
     _,X,X,X,X,X,X,X
     _,X,_,_,_,_,_,X
     _,X,_,X,_,X,_,X
     _,X,_,X,_,X,_,X
-    %,X,X,X,_,X,_,X
+    $,X,X,X,_,X,_,X
     !,^,_,_,_,X,_,_
     _,X,_,_,_,X,_,X
     X,X,X,X,X,X,X,X
@@ -28,11 +28,15 @@ public class MapTests {
     Where:
     ! = Hero (sam)
     ^ = Monster (Demonspawn)
-    % = Item (Health Potion)
+    $ = Item (Potion)
+
+    And the Monster drops the Amulet when killed
      */
 
-    @BeforeAll
-    public static void setUpMap() {
+    @BeforeEach
+    public void setUpMap() {
+        testMap = new Map("testMap");
+
         // Adds the hero at 0,5
         Position heroPos = new Position(0,5);
         testMap.setObjectAt(
@@ -40,8 +44,8 @@ public class MapTests {
                 new Hero(
                         '!',
                         "sam",
-                        5,
-                        5,
+                        40,
+                        10,
                         null,
                         "asdf",
                         new ArrayList<>()));
@@ -55,42 +59,171 @@ public class MapTests {
                 new Monster(
                         '^',
                         "Demonspawn",
-                        5,
-                        5,
+                        45,
+                        10,
                         null,
                         "Will hurt you! Better be prepared.",
-                        null
+                        new Item(
+                                '%',
+                                "Amulet",
+                                "Win the game!",
+                                "health",
+                                5)
                 ));
 
         Position itemPos = new Position(0,4);
         testMap.setObjectAt(
                 itemPos,
                 new Item(
-                        '%',
-                        "Health Potion",
-                        "Increases health by five",
+                        '$',
+                        "Potion",
+                        "Increases health by 10",
                         "health",
-                        5));
+                        10)
+                );
     }
 
-
-    @Test
     /**
      * @author Andrew Howes
      */
+    @Test
     void TestAllPossibleActions(){
         String allPos1 = testMap.allPossibleActions().toString();
+
         testMap.goRight();
         String allPos2 = testMap.allPossibleActions().toString();
+
         testMap.goDown();
         String allPos3 = testMap.allPossibleActions().toString();
 
-        String expected1 = "[(P) Pick up Health Potion - Increases health by five, (→) Turn right to face Demonspawn, (↓) Move down]";
-        String expected2 = "[(↑) Turn up to face Health Potion, (A) Attack Demonspawn, (↓) Move down]";
-        String expected3 = "[(↑) Move up]";
+        testMap.goUp();
+        String allPos4 = testMap.allPossibleActions().toString();
 
-        assertEquals(allPos1,expected1);
-        assertEquals(allPos2,expected2);
-        assertEquals(allPos3,expected3);
+        testMap.pickUpItem();
+        String allPos5 = testMap.allPossibleActions().toString();
+
+        testMap.goRight();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        String allPos6 = testMap.allPossibleActions().toString();
+
+        String expected1 = "[(P) Pick up Potion - Increases health by 10, (→) Turn right to face Demonspawn, (↓) Move down]";
+        String expected2 = "[(↑) Turn up to face Potion, (A) Attack Demonspawn, (↓) Move down]";
+        String expected3 = "[(↑) Move up]";
+        String expected4 = expected1;
+        String expected5 = "[(↑) Move up, (→) Turn right to face Demonspawn, (↓) Move down]";
+        String expected6 = "[(↑) Move up, (P) Pick up Amulet - Win the game!, (↓) Move down]";
+
+        assertEquals(expected1,allPos1);
+        assertEquals(expected2,allPos2);
+        assertEquals(expected3,allPos3);
+        assertEquals(expected4,allPos4);
+        assertEquals(expected5,allPos5);
+        assertEquals(expected6,allPos6);
+    }
+
+    /**
+     * Tests that the Boolean heroEscaped is set to null initially, and then
+     * after being killed it tests that heroEscaped is set to false;
+     * @author Andrew Howes
+     */
+    @Test
+    void HeroCanBeKilled() {
+        Boolean initial = testMap.heroEscaped;
+
+        testMap.goRight();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+
+        Boolean after = testMap.heroEscaped;
+
+        assertNull(initial);
+        assertFalse(after);
+    }
+
+    /**
+     * Tests that the hero can win by picking up the amulet.
+     */
+    @Test
+    void HeroCanWin() {
+        Boolean initial = testMap.heroEscaped;
+
+        testMap.pickUpItem();
+        testMap.goRight();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.attack();
+        testMap.pickUpItem();
+
+        Boolean after = testMap.heroEscaped;
+
+        assertNull(initial);
+        assertTrue(after);
+    }
+
+    /**
+     * Tests that hero can discard item after picking up and also tests the to string function
+     */
+    @Test
+    void canDiscardItem() {
+        String expectedWithItem = """
+                 #######
+                 #     #
+                 # # # #
+                 # # # #
+                $### # #
+                !^   # \s
+                 #   # #
+                ########""";
+
+        String expectedWithoutItem= """
+                 #######
+                 #     #
+                 # # # #
+                 # # # #
+                 ### # #
+                !^   # \s
+                 #   # #
+                ########""";
+
+        String mapBefore = testMap.toString();
+
+        testMap.pickUpItem();
+        String mapDuring = testMap.toString();
+
+        testMap.discardItem(0);
+        String mapAfter = testMap.toString();
+
+        assertEquals(expectedWithItem,mapBefore);
+        assertEquals(expectedWithoutItem,mapDuring);
+        assertEquals(expectedWithItem,mapAfter);
+    }
+
+    @Test
+    void writeReadTest() {
+        String firstMap = testMap.toString();
+        Map.save(99,"testMapSave");
+        new Map("99-testMapSave","99-testMapSave");
+        String firstReadMapString = testMap.toString();
+
+        setUpMap();
+
+        testMap.pickUpItem();
+        String secondMap = testMap.toString();
+        Map.save(99,"testMapSave");
+        new Map("99-testMapSave","99-testMapSave");
+        String secondReadMapString = testMap.toString();
+
+        setUpMap();
+
+        assertEquals(firstMap,firstReadMapString);
+        assertEquals(secondMap,secondReadMapString);
     }
 }
