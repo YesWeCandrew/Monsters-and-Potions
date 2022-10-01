7ff6b7fca2d121405f7bf1be757586f0fa76fb6e
Thomas Nouri-Azari <u7285974@anu.edu.au>
2022-10-02 07:24:15 +1100

    Added positions class unit tests

--- src/map/Position.java
+++ src/map/Position.java
@@ -1,9 +1,9 @@
 package map;
 
 /**
-This class allows us to store the position of important GameObjects, like the
-player so that they can be accessed easily. If we don't end up using it we can
-just remove it.
+ * This class allows us to store the position of important GameObjects, like the
+ * player so that they can be accessed easily. If we don't end up using it we can
+ * just remove it.
  */
 public class Position {
 
@@ -36,33 +36,40 @@ public class Position {
      * given the cardinality provided. For instance if facing East at
      * [3,4] moving forwards means going to position [4,4], but if facing North
      * at [3,4] moving forwards means going to position [3,3].
-     * @param cardinality the direction to move in
-     * @return the position one ahead of current in the directon specified
+     *
      * @author Andrew Howes
+     *
+     * @param cardinality the direction to move in
+     *
+     * @return the position one ahead of current in the directon specified
      */
     public Position positionInFront(Cardinality cardinality) {
         switch (cardinality) {
-            case NORTH -> {return new Position(x,y-1);}
-            case SOUTH -> {return new Position(x,y+1);}
-            case EAST -> {return new Position(x+1,y);}
-            default -> {return new Position(x-1,y);}
+            case NORTH -> { return new Position(x,y-1); }
+            case SOUTH -> { return new Position(x,y+1); }
+            case EAST -> { return new Position(x+1,y); }
+            case WEST -> { return new Position(x-1,y); }
+            default -> { return null; }
         }
     }
 
     /**
      * Returns the string instruction to move in the given direction
-     * @param cardinality the direction to move in
-     * @param action should be Move or Face. To describe type of action
-     * @return the string instruction to move that way
      *
      * @author Andrew Howes
+     *
+     * @param cardinality the direction to move in
+     * @param action should be Move or Face. To describe type of action
+     *
+     * @return the string instruction to move that way
      */
     public static String actions(Cardinality cardinality, String action) {
         switch (cardinality) {
-            case NORTH -> {return "(↑) "+action+" up";}
-            case EAST -> {return "(→) "+action+" right";}
-            case SOUTH -> {return "(↓) "+action+" down";}
-            default -> {return "(←) "+action+" left";}
+            case NORTH -> { return "(↑) "+action+" up"; }
+            case EAST -> { return "(→) "+action+" right"; }
+            case SOUTH -> { return "(↓) "+action+" down"; }
+            case WEST -> { return "(←) "+action+" left"; }
+            default -> { return null; }
         }
     }
 }
